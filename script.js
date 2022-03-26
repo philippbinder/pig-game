501382ace25f529f6da8c0ec2d4a946288b93201
Philipp Binder <88280928+philippbinder@users.noreply.github.com>
2022-03-26 22:28:14 +0100

    Add hold button and restart functionality

--- script.js
+++ script.js
@@ -22,49 +22,100 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-// scores array for both players to display their current scores
-let scores = [0, 0];
-// switch between players, 0 = player1, 1 = player2
-let currentPlayer = 0;
-let currentScore = 0;
+let scores, currentPlayer, currentScore, gameIsOngoing;
+
+// starting conditions -> create initialization function
+const init = function () {
+  // scores array for both players to display their current scores
+  scores = [0, 0];
+  // switch between players, 0 = player1, 1 = player2
+  currentPlayer = 0;
+
+  currentScore = 0;
+
+  // keep track of game state
+  gameIsOngoing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
+init();
+
+function switchPlayer() {
+  // switch player
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  // document
+  //   .querySelector(`.player--${currentPlayer}`)
+  //   .classList.remove('player--active');
+  currentScore = 0;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  // Add class if it's not there and remove it if it's there
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  // document
+  //   .querySelector(`.player--${currentPlayer}`)
+  //   .classList.add('player--active');
+}
 
 // dice  roll functionality
 // select dice button and add eventhandler
+
 // 1. random dice roll
 btnRoll.addEventListener('click', function () {
-  // generate a number from 1 to 6
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  if (gameIsOngoing) {
+    // generate a number from 1 to 6
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. display dice roll
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceRoll}.png`;
-  // 3. check if it is not a 1
-  if (diceRoll !== 1) {
-    currentScore += diceRoll;
-    // gets the current players text
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-    // }
-  } else {
-    // stwitch player
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    // document
-    //   .querySelector(`.player--${currentPlayer}`)
-    //   .classList.remove('player--active');
-    currentScore = 0;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    // Add class if it's not there and remove it if it's there
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    // document
-    //   .querySelector(`.player--${currentPlayer}`)
-    //   .classList.add('player--active');
+    // 2. display dice roll
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceRoll}.png`;
+    // 3. check if it is not a 1
+    if (diceRoll !== 1) {
+      currentScore += diceRoll;
+      // gets the current players text
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+      // }
+    } else {
+      switchPlayer();
+    }
+    console.log('rolled dice:', diceRoll);
+    console.log('scores array:', scores);
+    console.log('diceEl :', diceEl);
   }
-  console.log('rolled dice:', diceRoll);
-  console.log('scores array:', scores);
-  console.log('diceEl :', diceEl);
 });
+
+// save/hold score
+btnHold.addEventListener('click', function () {
+  if (gameIsOngoing) {
+    // use score arraay
+    scores[currentPlayer] += currentScore;
+    // add this score to the HTML element
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+    // end game if score is at least 100
+    if (scores[currentPlayer] >= 100) {
+      gameIsOngoing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+// reset game
+document.querySelector('.btn--new').addEventListener('click', init);
+// not init(), because the function is allready declared above. It is JS that calls this function as soon as the user clicks on the init button and therefore no ()
